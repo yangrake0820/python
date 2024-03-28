cf6f46d1898543981e93eaab209311e019e97f4e
DA9 <yangrake@gmail.com>
2024-03-28 16:53:29 +0900

    03/28

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render, redirect
 from .models import User
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
+from .forms import LoginForm
 
 def home(request):
-    return HttpResponse("Hello!")
+    user_id = request.session.get('user')
+    if user_id:
+        user = User.objects.get(pk = user_id)
+        return HttpResponse("Hello! %s님" % user)
+    else:
+        return HttpResponse("로그인 해주세요!")
 
 def register(request):
     if request.method == 'POST':
@@ -17,10 +23,10 @@ def register(request):
         
         if not(username and useremail and password and re_password):
             err_data['error'] = '모든 값을 입력해주세요.'
-        
+            return render(request, 'register.html', err_data)
         elif password != re_password:
             err_data['error'] = '비밀번호가 다릅니다.'
-        
+            return render(request, 'register.html', err_data)
         else:
             user = User(
                 username=username,
@@ -29,12 +35,21 @@ def register(request):
             )
             user.save()
 
-        return render(request, 'register.html', err_data)
+            return render(request, 'login.html', err_data)
     else:
         return render(request, 'register.html')    
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
 def logout(request):
-    pass
+    if request.session.get('user'):
+        del(request.session['user'])
+    return redirect('/')
